c8c1bb95aa32b28e3cb6f34d95c76204b174c8ce
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-27 12:53:23 +0530

    feat: add count live neighbours function to universe

--- convoys-game-of-life/game-of-life/src/lib.rs
+++ convoys-game-of-life/game-of-life/src/lib.rs
@@ -30,4 +30,21 @@ impl Universe {
     pub fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
+
+    // assuming torroidol grid(wrapped around)
+    pub fn count_live_neighbours(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                let nrow = (row + delta_row) % self.height;
+                let ncol = (col + delta_col) % self.width;
+                let idx = self.get_index(nrow, ncol);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
