b925a52be51a36f37c84aafdf1ab711917dc6a9c
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-27 13:21:46 +0530

    feat: add constructor and render to Universe

--- convoys-game-of-life/game-of-life/src/lib.rs
+++ convoys-game-of-life/game-of-life/src/lib.rs
@@ -29,6 +29,31 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+    
     pub fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
