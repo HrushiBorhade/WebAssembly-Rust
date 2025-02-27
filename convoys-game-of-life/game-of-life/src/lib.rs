154c2848a835e848f2aac8dea1f4a225a104c594
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-27 12:31:59 +0530

    feat: add cell and universe structure

--- convoys-game-of-life/game-of-life/src/lib.rs
+++ convoys-game-of-life/game-of-life/src/lib.rs
@@ -1,11 +1,33 @@
 use wasm_bindgen::prelude::*;
 mod utils;
+// #[wasm_bindgen]
+// extern "C" {
+//     pub fn alert(s: &str);
+// }
+
+// #[wasm_bindgen]
+// pub fn greet(msg: &str) {
+//     alert(&format!("{}", msg));
+// }
+
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 0,
 }
 
 #[wasm_bindgen]
-pub fn greet(msg: &str) {
-    alert(&format!("{}", msg));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
 }
